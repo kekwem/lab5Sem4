80c9fbf435d3d7c05627267f4d750c7ae0ca6c0e
kekwem <93526125+kekwem@users.noreply.github.com>
2022-04-25 23:57:52 +0200

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Sem4Lab5
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_dodaj_Click(object sender, RoutedEventArgs e)
+        {
+            double cena;
+            if (String.IsNullOrEmpty(imieTextBox.Text) || String.IsNullOrEmpty(nazwiskoTextBox.Text) ||
+                String.IsNullOrEmpty(cenaTextBox.Text))
+            {
+                MessageBox.Show("Brak wprowadzacych danych!");
+            }
+            else if (!Double.TryParse(cenaTextBox.Text, out cena))
+            {
+                MessageBox.Show("Trzeba wpisac liczbe!");
+            }
+            else if (kalend_wyp.SelectedDate == null || kalend_zwrot.SelectedDate == null)
+            {
+                MessageBox.Show("Wybierz daty wypozyczenia!");
+            }
+            else
+            {
+                if (kalend_wyp.SelectedDate > kalend_zwrot.SelectedDate || kalend_wyp.SelectedDate < kalend_wyp.DisplayDate ||
+                    kalend_zwrot.SelectedDate < kalend_wyp.DisplayDate)
+                {
+                    MessageBox.Show("Prawidlowo wybierz dayu!");
+                }
+                else
+                {
+                    DateTime dt1 =  kalend_wyp.SelectedDate.Value;
+                    DateTime dt2 = kalend_zwrot.SelectedDate.Value;
+                    Wypozyczenie wypozyczenie = new Wypozyczenie(imieTextBox.Text, nazwiskoTextBox.Text, cena,
+                        kaskBox.IsChecked.Value, gogleBox.IsChecked.Value, kijkiBox.IsChecked.Value, ((dt2-dt1).Days)+1);
+                    lista_osob.Items.Add(wypozyczenie);
+                }
+            }
+            
+        }
+
+        private void button_usun_Click(object sender, RoutedEventArgs e)
+        {
+            if (lista_osob.Items.IsEmpty)
+            {
+                MessageBox.Show("Brak zapisow w liscie!");
+            }
+            else if (lista_osob.SelectedItems.Count==0)
+            {
+                MessageBox.Show("Wybierz zapis ktory chcesz usunac!");
+            }
+            else
+            {
+                lista_osob.Items.RemoveAt(lista_osob.SelectedIndex);
+            }
+        }
+    }
+}
---
+++ Wypozyczenie.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sem4Lab5
+{
+    class Wypozyczenie
+    {
+        private string imie;
+        private string nazwisko;
+        private double cena_komplet;
+        private bool kaska;
+        private bool gogle;
+        private bool kijki;
+        private int liczba_dni;
+        public Wypozyczenie(string imie, string nazwisko, double cena_komplet, bool kaska, bool gogle, bool
+            kijki, int liczba_dni)
+        {
+            this.imie = imie;
+            this.nazwisko = nazwisko;
+            this.cena_komplet = cena_komplet;
+            this.kaska = kaska;
+            this.gogle = gogle;
+            this.kijki = kijki;
+            this.liczba_dni = liczba_dni;
+        }
+
+        double cena_wypozyczenia()
+        {
+            double caly_koszt = 0;
+            caly_koszt += cena_komplet;
+            if (kaska)
+            {
+                caly_koszt += 10;
+            }
+            if (gogle)
+            {
+                caly_koszt += 10;
+            }
+            if (kijki)
+            {
+                caly_koszt += 10;
+            }
+            return caly_koszt * liczba_dni;
+        }
+
+        public override string ToString()
+        {
+            return $"{imie} {nazwisko} ==> {cena_wypozyczenia()}";
+        }
+
+    }
+}
